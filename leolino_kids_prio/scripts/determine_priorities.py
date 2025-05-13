619e5ee85fc151db748786e61ba8569eee6a700c
Sebastian Ziesche <sziesche@gmx.de>
2025-05-13 23:56:00 +0200

    note used and unused spots

--- leolino_kids_prio/scripts/determine_priorities.py
+++ leolino_kids_prio/scripts/determine_priorities.py
@@ -2,12 +2,19 @@ from leolino_kids_prio.utils import Data
 
 
 if __name__ == '__main__':
-    age_group = "Ü3"
-    allowed_groups = ["B", "C", "D"]
+    date = "14. Mai 2025"
+    age_group = "U3"
+    allowed_groups = ["E", "F"]
 
     d = Data()
     allowed, nr, prio = d.allowed_and_prio(age_group, allowed_groups)
-    print(f"Today, the groups {allowed_groups} are can come to the daycare. I.e., the kids:")
+    print(f"Am {date} dürfen die Gruppen {allowed_groups} in die Kita kommen. Das heißt, die folgenden Kinder dürfen kommen:")
+    print(allowed)
+    print("can come.")
+    print(f"Es gibt {nr} freie Plätze, die mit Kindern von der Prioritätenliste gefüllt werden können. Diese sieht aktuell so aus:")
+    print(prio)
+    print("\n\n\n")
+    print(f"Tomorrow, the groups {allowed_groups} can come to the daycare. I.e., the kids:")
     print(allowed)
     print("can come.")
     print(f"There are {nr} free spots which can be filled with kids from the priority list, which currently looks like:")
