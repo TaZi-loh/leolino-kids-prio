1e436178e403a02fa11099008e5d4fa00168199c
Sebastian Ziesche <sziesche@gmx.de>
2025-07-16 01:07:47 +0200

    define new groups script

--- leolino_kids_prio/scripts/define_new_groups.py
+++ leolino_kids_prio/scripts/define_new_groups.py
@@ -5,16 +5,19 @@ from leolino_kids_prio.utils import Data, all_kids
 
 if __name__ == '__main__':
     new_u3_groups = {
-        "A": ["Lian", "Luca", "Khan", "Leia", "Giulio", "Thomas"],
-        "B": ["Ava", "Constantin", "Emma", "Leia", "Leonie"],
-        "C": ["Viola", "Robert", "Adya", "Leia", "Emilia"],
-        "D": ["Tino", "Lisa", "Marissa", "Leia", "Aaron"],
+        "A": ["Lian", "Luca", "Khan", "Leia"],
+        "B": ["Ava", "Thomas", "Emma", "Leia"],
+        "C": ["Viola", "Aaron", "Adya", "Leia"],
+        "D": ["Tino", "Lisa", "Marissa", "Leia"],
+        "E": ["Emilia", "Robert", "Constantin", "Leia"],
+        "F": ["Giulio", "Caiyi", "Leia"],
+        "G": ["Leonie", "Leia", "Holly"],
     }
     new_ue3_groups = {
-        "A": ["Benjamin", "Mila", "Sofia", "Chleo", "Sara", "Ana Laura", "Larissa", "Felix", "Zsigmond"],
-        "B": ["Conrad", "Ferdinand", "Nela", "Damian", "Kylian", "Valentina", "Lukas", "Valentin"],
-        "C": ["Liam", "Hayley Xin", "Mani", "Moritz", "Lea Amelie", "Emin", "Gabriel", "Karlo"],
-        "D": ["Jeremyas-Max", "Ananya", "Juna", "Yasmin", "Max", "Camille", "Lars"],
+        "A": ["Noah", "Sunny", "Leo", "Sofia", "Chleo", "Jeremyas-Max", "Ana Laura", "Larissa", "Felix", "Holly",],
+        "B": ["Conrad", "Ferdinand", "Ananya", "Damian", "Kylian", "Lukas", "Noah", "Valentina", "Nela", "Holly",],
+        "C": ["Noah", "Yasmin", "Hayley Xin", "Max", "Camille", "Lars", "Emin", "Gabriel", "Karlo", "Valentin", "Holly",],
+        "D": ["Sara", "Juna", "Yuan", "Liam", "Noah", "Mani", "Moritz", "Lea Amelie", "Jakob", "Taro", "Holly",],
     }
 
     d = Data()
@@ -24,7 +27,14 @@ if __name__ == '__main__':
     gone_kids = all_kids_old.difference(all_kids_new)
     print(f"The following kids are removed because they are in no group anymore: {gone_kids}")
     remove_kids(gone_kids)
-    print(f"The following kids should be added with the function `add_kid()`: {new_kids}")
+    new_kids_to_add = []
+    for new_kid in new_kids:
+        for file in (d.root / "generated" / "tiebreaker").glob("*.txt"):
+            with file.open("r") as f:
+                if new_kid not in f.read().split("\n"):
+                    new_kids_to_add.append(new_kid)
+    if len(new_kids_to_add) > 0:
+        raise ValueError(f"The following kids should be added with the function `add_kid()` first: {new_kids_to_add}")
 
     with (d.root / "manually_updated" / "groups.toml").open("w", encoding="utf-8") as f:
         toml.dump({"U3": new_u3_groups, "Ü3": new_ue3_groups}, f)
