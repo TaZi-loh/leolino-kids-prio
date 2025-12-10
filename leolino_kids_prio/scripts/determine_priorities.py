d9c32d7fe8b87fe8e8a964921a63e59572e3aa3d
Sebastian Ziesche <sziesche@gmx.de>
2025-12-10 21:57:33 +0100

    prepare the day

--- leolino_kids_prio/scripts/determine_priorities.py
+++ leolino_kids_prio/scripts/determine_priorities.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     """
     d = Data()
     full_ann = d.full_announcement_and_toml_update(
-        date="25.11.2025",
-        u3_groups=["1", "2", "4"],
+        date="10.12.2025",
+        u3_groups=["1", "2", "3"],
         # u3_groups=["1", "2", "3", "4"],
-        u3_stay_home_kids=["Aris", "Yian", "Marissa"],
+        u3_stay_home_kids=["Liam", "Tino"],
         # ue3_groups=["A", "B", "C", "D"],
-        ue3_groups=["A", "B", "C"],
-        ue3_stay_home_kids=["Jakob", "Moritz", "Emilia", ],
+        ue3_groups=["A", "C", "D"],
+        ue3_stay_home_kids=["Karlo", "Damian", "Aurelian", "Valentina"],
     )
     print(full_ann)
     print("\n\nupdating future prio list...")
