24a0db396556e2e5bb39210cf472468a56368977
Sebastian Ziesche <sziesche@gmx.de>
2026-04-10 22:46:21 +0200

    run update_prio_list_markdown.py

--- leolino_kids_prio/scripts/determine_priorities.py
+++ leolino_kids_prio/scripts/determine_priorities.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     """
     d = Data()
     full_ann = d.full_announcement_and_toml_update(
-        date="19.03.2026",
+        date="02.04.2026",
         u3_allowed_groups=["1", "2", "3", "4"],
         # u3_allowed_groups=["3", "4"],
         u3_stay_home_kids=[],
         # ue3_allowed_groups=["A", "B", "C", "D"],
         ue3_allowed_groups=["A", "B", "D"],
-        ue3_stay_home_kids=["Karlo", "Hailey", "Liam Ü3", "Moritz"],
+        ue3_stay_home_kids=["Tino", "Karlo", "Yasmin", "Liam Ü3", ],
     )
     print(full_ann)
     print("\n\nupdating future prio list...")
